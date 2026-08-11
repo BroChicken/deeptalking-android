f5148c893e2b60aca1960562f28f3701c0f93997
BroChicken <16228613+brochicken114@user.noreply.gitee.com>
2026-08-11 15:19:34 +0800

    feat: auto-open sidebar in WebView, immersive mode, seris app icon

--- android-lite/app/src/main/java/com/deeptalking/lite/MainActivity.kt
+++ android-lite/app/src/main/java/com/deeptalking/lite/MainActivity.kt
@@ -5,8 +5,12 @@ import android.app.Activity
 import android.app.AlertDialog
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.view.View
 import android.view.ViewGroup
+import android.view.WindowInsets
+import android.view.WindowInsetsController
 import android.webkit.JsResult
 import android.webkit.ValueCallback
 import android.webkit.WebChromeClient
@@ -110,7 +114,31 @@ class MainActivity : Activity() {
         setContentView(root)
         webView = wv
 
+        hideSystemBars()
+
         wv.loadUrl("file:///android_asset/hub.html")
+    }
+
+    private fun hideSystemBars() {
+        if (Build.VERSION.SDK_INT >= 30) {
+            window.insetsController?.let { controller ->
+                controller.systemBarsBehavior =
+                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+                controller.hide(WindowInsets.Type.navigationBars())
+            }
+        } else {
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        }
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) hideSystemBars()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
